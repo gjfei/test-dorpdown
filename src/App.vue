7da0e1dd13893103fcb5bd82f82045ce9f95417f
gjfei <1491180173@qq.com>
2022-07-04 09:49:54 +0800

    feat：init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <el-dropdown size="large" type="primary">
+    dropdown
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item>Action 1</el-dropdown-item>
+        <el-dropdown-item>Action 2</el-dropdown-item>
+        <el-dropdown-item>Action 3</el-dropdown-item>
+        <el-dropdown-item>Action 4</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+  <div style="height:1000px">
+    让下面的el-checkbox focus 然后鼠标移动到dropdrown上面页面会发生滚动
+  </div>
+  <el-checkbox />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
